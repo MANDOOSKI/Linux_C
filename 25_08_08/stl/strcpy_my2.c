95f1ff5d271fe4c637b0277e0ea7ee6d331d463a
Kanghyeon <gksrkdgus12@naver.com>
2025-08-10 19:53:26 +0900

    적용안된것들

--- 25_08_08/stl/strcpy_my2.c
+++ 25_08_08/stl/strcpy_my2.c
@@ -1,46 +1,53 @@
 #include <stdio.h>
 
-int length(const char* c){
+size_t length(const char* c){
 
-        int i = 0;
+	// null일경우 error
+	if(!c){
+		printf("error : null(1)");
+		return 1;
+	}
+
+        size_t i = 0;
         while(c[i] != '\0'){
-
-                i++;
-
+                ++i;
         }
 
         return i;
 
 }
 
-void strcpy_my(char* dest, char* src, size_t cap){
+int strcpy_my(char* dest, const char* src, size_t cap){
 
-	// dest의 크기가 잘못되있거나 src의 길이 > dest 용량일 경우 오류
-	if(0 >= cap || cap <= length(src)){
-		printf("error\n");
-		return;
+        // null인 경우 error
+	if(!dest || !src){
+		printf("error : null(1)\n");
+		return 1;
 	}
-
-	int i = 0;
-        while(1){
-
-                dest[i] = src[i];
-                if(src[i] == '\0'){
-                        break;
-                }
-                ++i;
-
+	
+	// 용량문제, 용량이 부족(dest < src길이)거나 용량설정이 잘못되있거나
+        size_t len = length(src);
+        if(0 == cap || cap < len + 1){
+                printf("error: capacity(2)\n");
+                return 2;
         }
+
+        while(*src){
+                *dest++ = *src++;
+        }
+        *dest = '\0';
+
+	return 0;
 
 }
 
 int main(){
 
         char dest[20] = "hello World";
-        char src[30] = "ByeByeByeByeByeByeBye";
+        char src[20] = "ByeBye";
 
-        int i = length(dest);
-        printf("%d\n", i);
+        size_t i = length(dest);
+        printf("dest length : %zu\n", i);
 
         printf("before : %s\n", dest);
         strcpy_my(dest, src, sizeof(dest));
